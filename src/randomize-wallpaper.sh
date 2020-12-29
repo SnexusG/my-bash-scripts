e137bcc31799e031f745f4c5716ce8fa72bac425
SnexusG <swapnilgore029@gmail.com>
2020-12-29 23:21:27 +0530

    new changes

--- src/randomize-wallpaper.sh
+++ src/randomize-wallpaper.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 
+
+while true;
+do
 DIR="/home/swapnil-gore/others/wallpapers"
 PIC=$(ls $DIR/* | shuf -n1)
 gsettings set org.gnome.desktop.background picture-uri "file://$PIC"
-
+sleep 300
+done
